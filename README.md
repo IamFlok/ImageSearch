7bfa8d8581294982a851b57110d7a635894fcbd0
Flok <xflokx@gmail.com>
2017-09-11 21:59:02 +0200

    finished Image search API
    - finished listing function
    - updated package.json

---
+++ README.md
@@ -0,0 +1,6 @@
+FCC Image search abstraction layer
+
+User stories:
+1) I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
+2) I can paginate through the responses by adding a ?offset=2 parameter to the URL.
+3) I can get a list of the most recently submitted search strings.
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,29 +15,63 @@ router.get(url, (req, res, next) => {
   var searchVal = req.params.imgSearch;
   var { offset } = req.query;
 
-  console.log("searchVal: " + searchVal);
-
+  // create a data which will be stored in the database
   var data = {
     term: searchVal,
     when: new Date()
   };
 
   updateToDatabase(data);
+  loadImages(searchVal, offset);
 
-  res.send(data);
+  // image loading function uses Google API
+  function loadImages(val, offset) {
+      gUrl = "https://www.googleapis.com/customsearch/v1?";
+      gKey =  process.env.API_KEY || "AIzaSyCV_3bYpOB4viJT_XQh-l_xpz0US-fpYyo";
+      gCx = process.env.CX || "007092322407064976168:kp_btmhckyu";
+      if (offset === undefined) {
+        gPath = gUrl + "key=" + gKey + "&cx=" + gCx + "&searchType=image" + "&q=" + val;
+      } else {
+        gPath = gUrl + "key=" + gKey + "&cx=" + gCx + "&searchType=image&start=" + offset + "&q=" + val;
+      }
+
+
+      request(gPath, (error, response, body) => {
+        if (error) throw error;
+
+        var resultToJSON = [];
+        var resultToReturn = [];
+
+        if (response.statusCode == 200) {
+          resultToJSON = JSON.parse(body).items;
+
+          if (resultToJSON) {
+            resultToJSON.forEach((result) => {
+              resultToReturn.push({
+                url: result.link,
+                snippet: result.snippet,
+                thumbnail: result.image.thumbnailLink,
+                context: result.image.contextLink
+              });
+            });
+            res.json(resultToReturn);
+          } else {
+            res.send("No data found");
+          }
+        } else res.send(response.statusCode);
+      });
+  };
 });
 
 // GET recent queries
-var queryUrl = "/api/latest/imagesearch/";
+var queryUrl = "/api/latest/imagesearch";
 
 router.get(queryUrl, (req, res, next) => {
   mongo.connect(process.env.MONGODB_URI ||
     'mongodb://fccuser:fCcUsER61@ds131384.mlab.com:31384/freecodecamp',
     (err, db) => {
       if (err) throw err;
-      var col = db.collection('imgsearch');
-
-      col.find({}, {
+      db.collection('imgsearch').find({}, {
         _id: false,
         term: true,
         when: true
@@ -48,21 +83,18 @@ router.get(queryUrl, (req, res, next) => {
     });
 });
 
-// helper function, updates passed object to database
+// updates a passed object to the database
 function updateToDatabase(obj) {
   mongo.connect(process.env.MONGODB_URI ||
     'mongodb://fccuser:fCcUsER61@ds131384.mlab.com:31384/freecodecamp',
     (err, db) => {
      if (err) throw err;
-     var col = db.collection('imgsearch');
-
-     col.insert(obj, (err, data) => {
+     db.collection('imgsearch').insert(obj, (err, data) => {
          if (err) throw err;
          console.log("Updated to database.");
          db.close();
      });
   });
 };
-
 
 module.exports = router;
